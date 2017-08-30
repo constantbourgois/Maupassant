f6858502ceaa470686334c9b972f04912849ded0
IsabelleTeran <isabelleteran@gmail.com>
2017-08-30 18:44:13 +0200

    lux

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -117,7 +117,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<path class="st0" d="M404.1 129c160 55.3 278.3-33.5 354.5 28.2" />
 					</g>
 					<g id="circles">
-						<g data-featherlight="#turgot-lbx" class="st2">
+                         <!-- fiucee -->
+						<g data-featherlight="#fiducee-lbx" class="st2">
 							<circle class="st1" cx="402.3" cy="129" r="65" />
 							<path class="st3" d="M397.5 96.4c1.6-.3 4-1.5 5.4-2.2l1.4-.7.3.5 3.6-4.4v-.1h-.1l-5.7.9.4.5s-3.1 1.5-4.7 2.3c-1.7.8-4.6 2-4.6 2s1.2 1.8 4 1.2"
 							/>
@@ -170,7 +171,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<path class="st3" d="M435.1 145.7c0-.6 0-.8-.2-.8h-.3v-.2h2.9l-.1.5s-.1 0-.1-.1-.1-.2-1-.2h-.6c-.1 0-.1 0-.1.1v1.3h.7c.6 0 .8 0 .8-.2v-.2h.2v1h-.2v-.2c0-.2-.1-.2-.7-.2h-.7v1c0 .7.1.7.2.7h.4c1 0 1.2-.1 1.3-.3 0 0 0-.1.1-.1v.1c0 .1-.1.3-.1.5h-2.7v-.2h.3c.1 0 .1 0 .1-.1v-2.4z"
 							/>
 						</g>
-						<g class="st2">
+                       <!-- leemo -->
+						<g data-featherlight="#leemo-lbx" class="st2">
 							<circle class="st4" cx="90.4" cy="90.4" r="90.4" />
 							<path id="SVGID_1_" d="M30.8 115.4h-.3l-1.8-4.2-1.8 4.2h-.3l-2.2-6.2h1.2l1.3 4 1.6-4h.3l1.7 4 1.4-4H33l-2.2 6.2zm11 0h-.3l-1.8-4.2-1.8 4.2h-.3l-2.2-6.2h1.2l1.3 4 1.6-4h.3l1.7 4 1.4-4H44l-2.2 6.2zm11 0h-.3l-1.8-4.2-1.8 4.2h-.3l-2.2-6.2h1.2l1.3 4 1.6-4h.3l1.7 4 1.4-4H55l-2.2 6.2zm6-.2c-.2.2-.4.3-.6.3s-.4-.1-.6-.3c-.2-.2-.3-.4-.3-.6 0-.2.1-.4.3-.6.2-.2.4-.3.6-.3s.4.1.6.3c.2.2.3.4.3.6 0 .2-.1.4-.3.6zm4.9-1.6v-6.9h1.1v6.7c0 .3.1.6.3.8.2.2.4.3.7.3v1c-1.4-.1-2.1-.7-2.1-1.9zm10.8-1.2h-4.4c0 .7.2 1.3.6 1.6.3.3.8.5 1.3.5.6 0 1.1-.2 1.5-.5l.5.8c-.2.2-.4.3-.8.4-.4.2-.9.2-1.4.2-.8 0-1.4-.3-2-.8-.6-.6-.9-1.3-.9-2.3 0-1 .3-1.8.9-2.4.5-.5 1.2-.8 1.9-.8.9 0 1.6.2 2 .7.5.5.7 1.1.7 1.9.2.3.1.5.1.7zm-1.4-1.9c-.3-.3-.7-.5-1.2-.5s-.9.2-1.2.5c-.3.3-.5.7-.5 1.1h3.4c-.1-.4-.2-.8-.5-1.1zm10.1 1.9h-4.4c0 .7.2 1.3.6 1.6.3.3.8.5 1.3.5.6 0 1.1-.2 1.5-.5l.5.8c-.2.2-.4.3-.8.4-.4.2-.9.2-1.4.2-.8 0-1.4-.3-2-.8-.6-.6-.9-1.3-.9-2.3 0-1 .3-1.8.9-2.4.5-.5 1.2-.8 1.9-.8.9 0 1.6.2 2 .7.5.5.7 1.1.7 1.9.2.3.1.5.1.7zm-1.4-1.9c-.3-.3-.7-.5-1.2-.5s-.9.2-1.2.5c-.3.3-.5.7-.5 1.1h3.4c-.1-.4-.2-.8-.5-1.1zm12 4.8v-3.9c0-1-.4-1.4-1.2-1.4-.3 0-.5.1-.7.2-.2.2-.4.3-.5.5v4.5h-1.1V111c0-.3-.1-.5-.3-.7-.2-.2-.5-.3-.9-.3-.2 0-.4.1-.7.2-.2.2-.4.4-.5.6v4.5h-1.1v-6.1h.7l.4.7c.4-.5.9-.8 1.6-.8.9 0 1.5.3 1.8.8.1-.2.3-.4.7-.6.3-.2.7-.2 1-.2.6 0 1.1.2 1.5.6.3.4.5.9.5 1.6v4.1h-1.2v-.1zm9.3-.7c-.5.6-1.2.9-2 .9-.9 0-1.5-.3-2-.9-.5-.6-.7-1.3-.7-2.3 0-.9.3-1.7.8-2.3.5-.6 1.2-.9 2-.9.9 0 1.6.3 2 .8.5.6.7 1.3.7 2.3 0 1.1-.3 1.8-.8 2.4zm-2-4.6c-.5 0-.9.2-1.2.6-.3.4-.4 1-.4 1.7 0 1.5.5 2.3 1.6 2.3.5 0 .9-.2 1.2-.6.3-.4.4-1 .4-1.7 0-1.5-.5-2.3-1.6-2.3zm8.1 5.2c-.2.2-.4.3-.6.3s-.4-.1-.6-.3c-.2-.2-.2-.4-.2-.6 0-.2.1-.4.2-.6.2-.2.4-.3.6-.3s.4.1.6.3c.2.2.3.4.3.6-.1.2-.1.4-.3.6zm8-7.5c-.2-.1-.4-.1-.6-.1-.3 0-.6.1-.8.4-.2.3-.3.6-.3 1v.3h1.2v.9h-1.2v5.2h-1.1v-5.2h-.9v-.9h.9c0-.8.2-1.4.6-1.8.4-.4.9-.7 1.5-.7.3 0 .7.1 1 .2l-.3.7zm6.7 2.6c-.2-.2-.5-.2-.7-.2-.4 0-.7.2-1 .5-.3.4-.4.8-.4 1.3v3.5h-1.1v-6.1h1.1v1c.4-.7 1-1.1 1.8-1.1.2 0 .5 0 .8.1l-.5 1z"
 							/>
@@ -202,7 +204,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								/>
 							</g>
 						</g>
-						<g class="st2">
+                         <!-- turgot -->
+						<g data-featherlight="#turgot-lbx" class="st2">
 							<circle class="st5" cx="441.5" cy="411.6" r="112.7" />
 							<path class="st3" d="M392.9 388.9c0 1-.2 1.9-.4 2.8 0 .1-.1.1-.2.1s-.2 0-.2-.1c-.2-.8-.4-1.4-.7-1.9-.3-.5-.7-.8-1.2-1.1-.5-.3-1-.5-1.6-.6-.6-.1-1.3-.2-1.9-.2h-5.5v26.8c0 1.2.4 2.3 1.2 3.1.8.9 1.7 1.4 2.9 1.6.1 0 .1.1.1.2s-.1.2-.2.2c-1.1-.1-2.1-.2-3.2-.2-1.1-.1-2.2-.1-3.3-.1-.8 0-1.6 0-2.4.1-.8 0-1.6.1-2.4.2-.1 0-.1 0-.2-.2 0-.1 0-.2.1-.2.7-.2 1.3-.3 1.7-.5.4-.2.7-.4.9-.7.2-.3.3-.7.4-1.2 0-.5.1-1.1.1-1.8V388h-7.6c-1 0-1.8.3-2.6 1-.7.7-1.3 1.5-1.7 2.3-.1.1-.1.1-.2.1s-.1-.1-.1-.2c-.1-.3-.1-.6-.1-.8 0-.6.1-1.2.2-1.8.1-.6.3-1.2.5-1.8l.3-.9c.1-.3.2-.6.2-.8.1-.2.2-.2.3-.1.6.4 1.3.7 1.9.8.6.1 1.3.2 2.1.2H390c.2 0 .5 0 .8-.1.3-.1.6-.1.8-.2.2 0 .3-.1.5-.1s.4-.1.6-.1c.1 0 .1.1.1.2v.4c.1.8.1 1.7.1 2.8z"
 							/>
@@ -249,8 +252,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								/>
 							</g>
 						</g>
+                         <!-- vingtrois-->
 
-						<g class="st2">
+						<g data-featherlight="#vingtrois-lbx"  class="st2">
 							<circle class="st6" cx="780.7" cy="408.5" r="57.2" />
 							<path class="st3" d="M750.9 434.8l-3.3-8h-.8l4 9.6h.2l4-9.6h-.8z" />
 							<path class="st3" d="M756.1 426.8h.7v9.5h-.7z" />
@@ -275,7 +279,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<path class="st3" d="M794.8 395.1v4.7c2.1 0 3.7.3 4.7.9.2.1.4.2.5.4.2-1.1.3-2.3.3-3.5 0-1.2-.1-2.4-.3-3.6-.2.2-.4.3-.6.4-.7.5-2.3.7-4.6.7z"
 							/>
 						</g>
-						<g class="st2">
+                         <!-- fundee -->
+						<g data-featherlight="#fundee-lbx"  class="st2">
 
 
 							<circle class="st7" cx="112.7" cy="386" r="79.8" />
@@ -299,8 +304,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<path class="st3" d="M103.3 388.6h3.4v8.2h-3.4z" />
 
 						</g>
+                         <!-- orelis-->
 
-						<g class="st2">
+						<g data-featherlight="#orelis-lbx"  class="st2">
 							<circle class="st8" cx="758.5" cy="157.2" r="99.4" />
 							<path class="st3" d="M698.6 153.8c-.3-.8-.7-1.5-1.2-2s-1.1-.9-1.8-1.2c-.7-.3-1.4-.4-2.2-.4-.8 0-1.5.1-2.2.4-.7.3-1.3.6-1.8 1.2-.5.5-.9 1.2-1.2 2-.3.8-.4 1.8-.4 2.9 0 1.1.1 2.1.4 2.9.3.8.7 1.5 1.2 2s1.1.9 1.8 1.2c.7.3 1.4.4 2.2.4.8 0 1.5-.1 2.2-.4.7-.3 1.3-.6 1.8-1.2.5-.5.9-1.2 1.2-2 .3-.8.4-1.8.4-2.9 0-1.1-.1-2.1-.4-2.9m-12.1-.1c.4-.9.9-1.6 1.5-2.2.7-.6 1.4-1.1 2.4-1.4.9-.3 1.9-.5 3-.5s2.1.2 3 .5c.9.3 1.7.8 2.4 1.4.7.6 1.2 1.4 1.5 2.2.4.9.6 1.9.6 3s-.2 2.1-.6 3c-.4.9-.9 1.6-1.5 2.3-.7.6-1.4 1.1-2.4 1.4-.9.3-1.9.5-3 .5s-2.1-.2-3-.5c-.9-.3-1.7-.8-2.4-1.4-.7-.6-1.2-1.4-1.5-2.3-.4-.9-.6-1.9-.6-3 .1-1.1.3-2.1.6-3"
 							/>
@@ -478,8 +484,83 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre ap
 							</p>
 							</span>
-						<a href="#">aller au site pour plus d'informations</a>
+						<a target="_blank" href="http://www.turgot-am.fr/">aller au site pour plus d'informations</a>
 						<span class="imglbx"><img src="assets/img/logoturgottransp.png" alt="turgotlbx"></span>
+						</span>
+
+					</div>
+				</div>
+        <div id="lightboxes">
+					<div class="lbx5" id="fiducee-lbx">
+						<span class="innerlbx">
+							<span class="paraglbx">
+							<h5>Une gestion diversifiée, indépendante et reconnue</h5>
+							<p>Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre approche de la gestion financière se décline sous différents formats pour être en permanence au plus proche du profil de nos clients investisseurs et des attentes particulières qui les caractérisent
+	Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre ap
+							</p>
+							</span>
+						<a target="_blank" href="http://www.fiducee.com/">aller au site pour plus d'informations</a>
+						<span class="imglbx5"><img src="assets/img/logoturgottransp_0000_fiduceelogo.png" alt="fiduceelbx"></span>
+						</span>
+
+					</div>
+				</div>
+        <div id="lightboxes">
+					<div class="lbx2" id="orelis-lbx">
+						<span class="innerlbx">
+							<span class="paraglbx">
+							<h5>Une gestion diversifiée, indépendante et reconnue</h5>
+							<p>Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre approche de la gestion financière se décline sous différents formats pour être en permanence au plus proche du profil de nos clients investisseurs et des attentes particulières qui les caractérisent
+	Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre ap
+							</p>
+							</span>
+						<a target="_blank" href="https://www.orelis.fr/">aller au site pour plus d'informations</a>
+						<span class="imglbx2"><img src="assets/img/logoturgottransp_0003_orelislogo.png" alt="orelislbx"></span>
+						</span>
+
+					</div>
+				</div>
+        <div id="lightboxes">
+					<div class="lbx4" id="fundee-lbx">
+						<span class="innerlbx">
+							<span class="paraglbx">
+							<h5>Une gestion diversifiée, indépendante et reconnue</h5>
+							<p>Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre approche de la gestion financière se décline sous différents formats pour être en permanence au plus proche du profil de nos clients investisseurs et des attentes particulières qui les caractérisent
+	Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre ap
+							</p>
+							</span>
+						<a target="_blank" href="http://www.fundee.fr/">aller au site pour plus d'informations</a>
+						<span class="imglbx4"><img src="assets/img/logoturgottransp_0002_fundeelogo.png" alt="fundeelbx"></span>
+						</span>
+
+					</div>
+				</div>
+        <div id="lightboxes">
+					<div class="lbx3" id="vingtrois-lbx">
+						<span class="innerlbx">
+							<span class="paraglbx">
+							<h5>Une gestion diversifiée, indépendante et reconnue</h5>
+							<p>Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre approche de la gestion financière se décline sous différents formats pour être en permanence au plus proche du profil de nos clients investisseurs et des attentes particulières qui les caractérisent
+	Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre ap
+							</p>
+							</span>
+						<a target="_blank" href="http://www.vingtrois.fr/" >aller au site pour plus d'informations</a>
+						<span class="imglbx3"><img src="assets/img/logoturgottransp_0001_23logo.png" alt="vingtroislbx"></span>
+						</span>
+
+					</div>
+				</div>
+        <div id="lightboxes">
+					<div class="lbx" id="leemo-lbx">
+						<span class="innerlbx">
+							<span class="paraglbx">
+							<h5>Une gestion diversifiée, indépendante et reconnue</h5>
+							<p>Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre approche de la gestion financière se décline sous différents formats pour être en permanence au plus proche du profil de nos clients investisseurs et des attentes particulières qui les caractérisent
+	Turgot Asset Management gère une gamme sélective de produits investis sur les principaux marchés d’actions et d’obligations pour le compte de clients privés et institutionnels. Notre ap
+							</p>
+							</span>
+						<a target="_blank" href="#">aller au site pour plus d'informations</a>
+						<span class="imglbx"><img src="assets/img/logoturgottransp.png" alt="leemolbx"></span>
 						</span>
 
 					</div>
