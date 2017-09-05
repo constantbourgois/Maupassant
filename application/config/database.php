b190ce606dfa6e64231474b740143316e5a0e983
CONSTANT“ <constantbourgois@gmail.com>
2017-09-05 11:24:33 +0200

    otro

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => 'root',
-	'database' => 'maupassant_events',
+	'hostname' => 'constantbourgois.com',
+	'username' => 'mtruckhostin_mtruckhostin',
+	'password' => 'MTruck4ever',
+	'database' => 'mtruckhostin_maupassantevents',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
