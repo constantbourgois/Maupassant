d6218d87b4395ce6da12d47acc90b4210a046686
IsabelleTeran <isabelleteran@gmail.com>
2017-09-18 15:33:04 +0200

    des

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -78,7 +78,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			</nav>
 			<div class="fondh1">
 
-				<span><h1>Maupassant</h1>
+				<span><h1>Hotel de <br> Maupassant</h1>
 			<h2> <span>
 Leader dans l’accompagnement<br>des cabinets de
 gestion de patrimoine.</span></h2>
